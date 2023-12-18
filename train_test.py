385e587bb069de7288a3ddb312bbc5097410f5b0
Mateusz <mateusz.adamowicz.lo1@gmail.com>
2023-12-18 22:18:06 +0100

    Updating README.md

--- train_test.py
+++ train_test.py
@@ -16,48 +16,10 @@ rnn = RNN(hyperparameters)
 loss_history = rnn.train(train_set, valid_set)
 rnn.save_weights(TRAINED_NETWORK_PATH)
 
-visualizer = Visualizer(VISUALIZER_SIZE, VISUALIZER_COLUMNS)
+visualizer = Visualizer(3, 2)
 
-test_x, test_y = test_set
-hiddens, predictions = rnn.forward(test_x)
-loss_history = [mse(y, pred) for y, pred in zip(test_y, predictions)]
-
-train_x, train_y = train_set
-train_hiddens, train_predictions = rnn.forward(train_x)
-train_history = [mse(y, pred) for y, pred in zip(train_x, train_predictions)]
-
-valid_x, valid_y = valid_set
-valid_hiddens, valid_predictions = rnn.forward(valid_x)
-valid_history = [mse(y, pred) for y, pred in zip(valid_x, train_predictions)]
-
-visualizer = Visualizer(2, 2)
-
-visualizer.draw(
-    [(loss_history, "Cost Function")],
-    "Epoch",
-    "Loss",
-    "Cost Function"
-)
-
-visualizer.draw(
-    [(train_predictions, "Predictions"), (train_y, "Actual Value")],
-    "X",
-    "Y",
-    "Post-Training"
-)
-
-visualizer.draw(
-    [(predictions, "Predictions"), (test_y, "Actual Value")],
-    "X",
-    "Y",
-    "Test"
-)
-
-visualizer.draw(
-    [(valid_predictions, "Predictions"), (valid_y, "Actual Value")],
-    "X",
-    "Y",
-    "Valid Test"
-)
+visualizer.create_chart_set(train_set, rnn, "Post-Training Test")
+visualizer.create_chart_set(valid_set, rnn, "Validation Test")
+visualizer.create_chart_set(test_set, rnn, "New Data Training Test")
 
 visualizer.visualize()
